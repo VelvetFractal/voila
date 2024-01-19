2ee9c2c8df2b6095cd063cbaa7e8b9df38820c9c
VelvetFractal <alloetotixd@gmail.com>
2024-01-19 23:11:57 +0100

    almost done with web3

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from web3 import Web3
 import time
+from decimal import Decimal
+
 
 w3 = Web3(Web3.HTTPProvider('key'))
+
 
 wallet_address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 
@@ -12,7 +15,8 @@ def get_transaction_info(block_number, wallet_address):
     block = w3.eth.get_block(block_number, full_transactions=True)
     
     for tx in block.transactions:
-        if wallet_address.lower() == tx['to'].lower() or wallet_address.lower() == tx['from'].lower():
+        if (tx['to'] and wallet_address.lower() == tx['to'].lower()) or \
+           (tx['from'] and wallet_address.lower() == tx['from'].lower()):
             return {
                 'from': tx['from'],
                 'to': tx['to'],
@@ -21,6 +25,17 @@ def get_transaction_info(block_number, wallet_address):
             }
 
     return None
+
+def check_wallet_balance(wallet_address, previous_balance):
+    current_balance = w3.eth.get_balance(wallet_address)
+    
+    if current_balance != previous_balance:
+        balance_change = current_balance - previous_balance
+        print(f"Balance change detected: {balance_change} Wei")
+        print(f"Current Balance: {current_balance} Wei")
+    
+    return current_balance
+
 
 def check_wallet_activity(wallet_address):
     latest_block_number = get_latest_block_number()
@@ -35,17 +50,18 @@ def check_wallet_activity(wallet_address):
             print(f"Value: {transaction_info['value']} Ether")
             return
         else:
-            print(f"none activity detected in block {transaction_info['block_number']}")
+            print("none activity detected in block")
             return
 
     print("No recent activity found for the wallet.")
 
 if __name__ == "__main__":
     try:
+        previous_balance = w3.eth.get_balance(wallet_address)
+
         while True:
             check_wallet_activity(wallet_address)
-            time.sleep(12)  
+            previous_balance_wei = check_wallet_balance(wallet_address, previous_balance)
+            time.sleep(12) 
     except KeyboardInterrupt:
         print("Program stopped by user.")
-
-def 

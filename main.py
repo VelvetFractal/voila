a9dcfc2122071dd5fb877960a3386a6921b570d1
VelvetFractal <alloetotixd@gmail.com>
2024-01-21 18:45:13 +0100

    final program

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import telebot
 from web3 import Web3
 import time
-from decimal import Decimal
+import threading
 
 
-w3 = Web3(Web3.HTTPProvider('key'))
+TELEGRAM_BOT_TOKEN = '6775443880:AAE0RhxESCup7zyyCLnu2hrX7cjPd_-QypY'
+stop_flag = False
 
+bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
+w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/bc2ee063a84741f3babf0e1dacf46e7d'))
 
-wallet_address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(message.chat.id, 'Привет, введите кошелек для мониторинга')
 
 def get_latest_block_number():
     return w3.eth.block_number
@@ -26,42 +32,61 @@ def get_transaction_info(block_number, wallet_address):
 
     return None
 
-def check_wallet_balance(wallet_address, previous_balance):
+def check_wallet_balance(wallet_address, previous_balance, message):
     current_balance = w3.eth.get_balance(wallet_address)
     
     if current_balance != previous_balance:
         balance_change = current_balance - previous_balance
-        print(f"Balance change detected: {balance_change} Wei")
-        print(f"Current Balance: {current_balance} Wei")
+        balance_change_in_ether = w3.from_wei(abs(balance_change), 'ether')
+        bot.send_message(message.chat.id,f"""Balance change detected: {balance_change_in_ether} ether\nCurrent Balance: {w3.from_wei(abs(current_balance), 'ether')} ether""")
+        return True
     
-    return current_balance
+        
+    return False
 
 
-def check_wallet_activity(wallet_address):
+def check_wallet_activity(wallet_address, message):
     latest_block_number = get_latest_block_number()
 
     for block_number in range(latest_block_number, max(0, latest_block_number - 10), -1):
         transaction_info = get_transaction_info(block_number, wallet_address)
         
         if transaction_info:
-            print(f"Wallet activity detected in block {transaction_info['block_number']}:")
-            print(f"From: {transaction_info['from']}")
-            print(f"To: {transaction_info['to']}")
-            print(f"Value: {transaction_info['value']} Ether")
-            return
-        else:
-            print("none activity detected in block")
-            return
+            return transaction_info
+    return None    
+        
 
-    print("No recent activity found for the wallet.")
+@bot.message_handler(commands=['stop'])
+def handle_stop(message):
+    global stop_flag
+    bot.reply_to(message, '))')
+    bot.send_message(message.chat.id, 'Enter adress to monitor it')
+    stop_flag = True    
+
+
+@bot.message_handler(content_types=['text'])   
+def main_work(message):
+    global stop_flag
+    stop_flag = False 
+    if len(message.text.strip().upper()) != 42:
+        bot.send_message(message.chat.id, 'This is not an ETH address')
+    bot.send_message(message.chat.id, 'Im starting ')    
+    wallet_address = message.text.strip()
+    previous_balance = w3.eth.get_balance(wallet_address)
+    transaction_info = ''
+    while  not stop_flag:
+            if check_wallet_balance(wallet_address, previous_balance, message):
+                previous_balance = w3.eth.get_balance(wallet_address)
+            if transaction_info != check_wallet_activity(wallet_address, message) and check_wallet_activity(wallet_address, message) != None:
+                transaction_info = check_wallet_activity(wallet_address, message)
+                bot.send_message(message.chat.id, f"""Wallet activity detected in block: {transaction_info['block_number']}\nFrom: {transaction_info['from']}\nTo: {transaction_info['to']}\nValue: {transaction_info['value']} Ether""")
+            time.sleep(12)
+    
 
 if __name__ == "__main__":
-    try:
-        previous_balance = w3.eth.get_balance(wallet_address)
+    
 
-        while True:
-            check_wallet_activity(wallet_address)
-            previous_balance_wei = check_wallet_balance(wallet_address, previous_balance)
-            time.sleep(12) 
+    try:
+        bot.polling(none_stop=True)
     except KeyboardInterrupt:
-        print("Program stopped by user.")
+        print ("Program has ended by user")
